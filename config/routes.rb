2129968f24ad1119fb2730675f1275709ac7094d
Noa Udler <udlernoa4@gmail.com>
2022-01-31 15:07:07 -0800

    data

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  get '/hello', to: 'application#hello_world'
+  
+  resources :equipment
+  resources :categories
+  resources :comments
+  resources :games
+  resources :users_games
+  resources :users
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
